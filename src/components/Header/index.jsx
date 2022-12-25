470c25bd89d7dc5e2c3e8e8230cac6277f0f1ff6
SolomonOjigbo <solomonojigbo@gmail.com>
2022-12-25 15:00:34 +0100

    Designed the Header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import {
+	AppBar,
+	Autocomplete,
+	InputBase,
+	Toolbar,
+	Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+
+import useStyles from "./styles";
+import { Search } from "@mui/icons-material";
 
 const Header = () => {
-	return <div>Header</div>;
+	const classes = useStyles();
+
+	return (
+		<AppBar position="static">
+			<Toolbar className={classes.toolbar}>
+				<Typography variant="h5" className={classes.title}>
+					Travel Advisor
+				</Typography>
+				<Box display="flex">
+					<Typography variant="h6" className={classes.title}>
+						Explore new Places
+					</Typography>
+					{/* <Autocomplete> */}
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+							<Search />
+						</div>
+						<InputBase
+							placeholder="Search…"
+							classes={{ root: classes.inputRoot, input: classes.inputInput }}
+						/>
+					</div>
+					{/* </Autocomplete> */}
+				</Box>
+			</Toolbar>
+		</AppBar>
+	);
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+const theme = createTheme();
 root.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
