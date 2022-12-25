bbf83566e1a1043946ce6afff813744cd1f379da
SolomonOjigbo <solomonojigbo@gmail.com>
2022-12-25 07:39:05 +0100

    added the Header, Map, List, and PlaceDetail components to the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Header from "./components/Header";
+import List from "./components/List";
+import Map from "./components/Map";
+
 function App() {
-	return <main className="App">Travel Advisor</main>;
+	return (
+		<>
+			<CssBaseline />
+			<Header />
+			<Grid container spacing={3} style={{ width: "100%" }}>
+				<Grid item xs={12} md={4}>
+					<List />
+				</Grid>
+				<Grid item xs={12} md={8}>
+					<Map />
+				</Grid>
+			</Grid>
+		</>
+	);
 }
 
 export default App;
