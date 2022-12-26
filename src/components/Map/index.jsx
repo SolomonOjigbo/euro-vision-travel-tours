d41f2469527f4064cd368388fd7ebf956f831084
SolomonOjigbo <solomonojigbo@gmail.com>
2022-12-26 01:48:36 +0100

    Added Google maps to the Map component

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@mui/material";
+import { LocationOnOutlined } from "@mui/icons-material";
+import Rating from "@mui/material";
+
+import useStyles from "./styles";
+import mapStyles from "../../mapStyles";
 
 const Map = () => {
-	return <div>Map</div>;
+	const classes = useStyles();
+	const isMobile = useMediaQuery("(min-width:600px)");
+
+	const coordinates = { lat: 0, lng: 0 };
+
+	return (
+		<div className={classes.mapContainer}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
+				defaultCenter={coordinates}
+				center={coordinates}
+				defaultZoom={14}
+				margin={[50, 50, 50, 50]}
+				options={{
+					disableDefaultUI: true,
+					zoomControl: true,
+					styles: mapStyles,
+				}}
+			></GoogleMapReact>
+		</div>
+	);
 };
 
 export default Map;
